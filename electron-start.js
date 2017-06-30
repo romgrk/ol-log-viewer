2cbef0db82e90b85f6de6210cedb7c8ce60cb9fd
Rom Grk <romgrk.cc@gmail.com>
2017-06-30 16:45:15 -0400

    increase window default width

--- electron-start.js
+++ electron-start.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1000, height: 600})
 
   // Load the index.html of the app.
   const startUrl = process.env.ELECTRON_START_URL || url.format({
